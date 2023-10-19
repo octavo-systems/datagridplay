14411d315911f68b3c6ed6e2eb3641868e9c03d9
aw@rationalcommerce.com <aw@rationalcommerce.com>
2023-10-19 14:18:11 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,4 +7,5 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.tbl.add_component('one') 
+    self.sifterrows.items = [{'sifter':'adrian'}]
+    
