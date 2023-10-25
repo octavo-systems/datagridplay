95b72104becb64495e5d527c8cf174262d8664fb
aw@rationalcommerce.com <aw@rationalcommerce.com>
2023-10-25 10:24:34 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,4 +8,20 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
-    self.sifterrows.items = [{'sifter':'adrian'}, {'sifter':'Roy'}]
+    #self.sifterrows.items = [{'sifter':'adrian'}, {'sifter':'Roy'}]
+
+    critical = [{'sifter':'adrian'}]
+    high = [{'sifter':'Roy'}]
+    normal = [{'sifter':'Mark'}]
+    low = [{'sifter':'Wojtek'}]
+    trivial = [{'sifter':'Naked Ape'}]
+
+    collection = critical+high+normal
+
+    self.sifterrows.items = collection 
+    #self.sifterrows.items.extend( high )
+    #self.sifterrows.items.extend( normal )
+    #self.sifterrows.items.extend( low )    
+    #self.sifterrows.items.extend( trivial )
+
+
